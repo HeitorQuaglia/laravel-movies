2d4afc952e35e67dbfa03bb933541060ab66f3e8
Heitor Quaglia <heitor.bq@hotmail.com>
2020-06-22 20:27:52 -0300

    add show, navbar and route

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,7 +7,18 @@
 
     <link rel="stylesheet" href="/css/main.css">
 </head>
-<body>
+<body class="font-sans bg-gray-900 text-white">
+    <nav class="border-b border-gray-800">
+        <div class="container mx-auto items-center justify-between px-4 py-6">
+            <ul class="flex items-center">
+                <li>
+                    <a href="#">
 
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    @yield('content')
 </body>
 </html>
